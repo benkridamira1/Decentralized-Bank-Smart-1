12759a2d80dc8ae7829d0c5242a5edf084438b0c
Amira <benkridamira14@gmail.com>
2022-12-22 23:51:31 +0100

    add varaibles and mapping to the contract

--- DEFI/contracts/DefiBank.sol
+++ DEFI/contracts/DefiBank.sol
@@ -1,2 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+
+contract DefiBank {
+
+     address public usdc;
+    address public ausd;
+
+    address[] public stakers;
+    
+    mapping(address => uint) public depositBalance;
+    mapping(address => bool) public hasDeposited;
+
+     constructor(address _usdc, address _ausd) {
+        usdc = _usdc;
+        ausd = _ausd;
+    }
+}
