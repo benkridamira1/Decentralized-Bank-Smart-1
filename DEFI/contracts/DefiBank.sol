6ee57dc9a71040fc1f7e978454a0dea18a5e4577
Amira <benkridamira14@gmail.com>
2022-12-22 23:53:52 +0100

    add withdraw method

--- DEFI/contracts/DefiBank.sol
+++ DEFI/contracts/DefiBank.sol
@@ -2,6 +2,22 @@
 pragma solidity ^0.8.0;
 
 
+
+interface IERC20 {
+    function totalSupply() external view returns (uint);
+    function balanceOf(address account) external view returns (uint);
+    function transfer(address recipient, uint amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint);
+    function approve(address spender, uint amount) external returns (bool);
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint amount
+    ) external returns (bool);
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+}
+
 contract DefiBank {
 
      address public usdc;
@@ -26,5 +42,18 @@ contract DefiBank {
         }
 
         hasDeposited[msg.sender] = true;
+    }
+
+
+    function withdraw(uint _amount) public {
+        uint balance = depositBalance[msg.sender];
+        require(balance > 0, "Balance cannot be 0");
+        IERC20(usdc).transfer(msg.sender, _amount);
+
+        depositBalance[msg.sender] -= _amount;
+
+        if (balance - _amount == 0) {
+            hasDeposited[msg.sender] = false;
+        }
     }
 }
